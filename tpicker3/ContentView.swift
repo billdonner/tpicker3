79d70feae90804e0446be71eee93e0ef40f708b9
bill donner <billdonner@gmail.com>
2024-07-05 17:15:07 -0500

    Initial Commit

---
+++ tpicker3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tpicker3
+//
+//  Created by bill donner on 7/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tpicker3/tpicker3App.swift
@@ -0,0 +1,17 @@
+//
+//  tpicker3App.swift
+//  tpicker3
+//
+//  Created by bill donner on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct tpicker3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
